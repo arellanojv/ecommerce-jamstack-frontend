c96a5daf0c4788cf4e3a197a5c079ea8109f3f6c
Jhon Arellano <jah4n@Jhons-MacBook-Air.local>
2022-04-25 12:19:45 +0800

    Recently Viewed Products UI and Functionality

--- src/templates/ProductDetail.js
+++ src/templates/ProductDetail.js
@@ -1,30 +1,64 @@
 import React, { useState, useEffect } from "react"
 import Grid from "@material-ui/core/Grid"
+import useMediaQuery from "@material-ui/core/useMediaQuery"
 
 import Layout from "../components/ui/layout"
 import ProductImages from "../components/product-detail/ProductImages"
 import ProductInfo from "../components/product-detail/ProductInfo"
+import RecentlyViewed from "../components/product-detail/RecentlyViewed"
 
 export default function ProductDetail({
-  pageContext: { name, id, category, description, variants },
+  pageContext: { name, id, category, description, variants, product },
 }) {
   const [selectedVariant, setSelectedVariant] = useState(0)
   const [selectedImage, setSelectedImage] = useState(0)
 
-  useEffect(() => {
-    const params = new URLSearchParams(window.location.search)
+  const matchesMD = useMediaQuery(theme => theme.breakpoints.down("md"))
 
+  const params = new URLSearchParams(window.location.search)
+  const style = params.get("style")
+
+  useEffect(() => {
     const styledVariant = variants.filter(
       variant => variant.style === params.get("style")
     )[0]
 
-    setSelectedVariant(variants.indexOf(styledVariant))
-  }, [])
+    const variantIndex = variants.indexOf(styledVariant)
+
+    var recentlyViewed = JSON.parse(
+      window.localStorage.getItem("recentlyViewed")
+    )
+
+    if (recentlyViewed) {
+      if (recentlyViewed.length === 10) {
+        recentlyViewed.shift()
+      }
+
+      if (
+        !recentlyViewed.some(
+          product =>
+            product.node.name === name &&
+            product.selectedVariant === variantIndex
+        )
+      ) {
+        recentlyViewed.push({ ...product, selectedVariant: variantIndex })
+      }
+    } else {
+      recentlyViewed = [{ ...product, selectedVariant: variantIndex }]
+    }
+
+    window.localStorage.setItem(
+      "recentlyViewed",
+      JSON.stringify(recentlyViewed)
+    )
+
+    setSelectedVariant(variantIndex)
+  }, [style])
 
   return (
     <Layout>
       <Grid container direction="column">
-        <Grid item container>
+        <Grid item container direction={matchesMD ? "column" : "row"}>
           <ProductImages
             images={variants[selectedVariant].images}
             selectedImage={selectedImage}
@@ -38,6 +72,9 @@ export default function ProductDetail({
             setSelectedVariant={setSelectedVariant}
           />
         </Grid>
+        <RecentlyViewed
+          products={JSON.parse(window.localStorage.getItem("recentlyViewed"))}
+        />
       </Grid>
     </Layout>
   )
